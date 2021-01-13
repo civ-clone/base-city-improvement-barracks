f2b6bf2b765cedefc9c72d70da6f3f56129b181d
Dom Hastings <dom111@users.noreply.github.com>
2021-01-13 20:24:42 +0000

    Initial commit

---
+++ Barracks.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Barracks = void 0;
+const CityImprovement_1 = require("@civ-clone/core-city-improvement/CityImprovement");
+class Barracks extends CityImprovement_1.default {
+}
+exports.Barracks = Barracks;
+exports.default = Barracks;
+//# sourceMappingURL=Barracks.js.map
---
+++ Barracks.ts
@@ -0,0 +1,5 @@
+import CityImprovement from '@civ-clone/core-city-improvement/CityImprovement';
+
+export class Barracks extends CityImprovement {}
+
+export default Barracks;
